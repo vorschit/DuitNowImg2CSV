ebd8d5c7ab920231c0769ad373c6e3f062b20a2b
Muhammad Adiib <81273210+vorschit@users.noreply.github.com>
2024-10-15 15:12:33 +0800

    Testing If else regex not match

--- img2text.py
+++ img2text.py
@@ -3,6 +3,13 @@ from pytesseract import pytesseract
 import glob
 import os
 import re
+import pandas
+
+Rm=[]
+date = []
+merchantName= []
+fileNameError= []
+null="NULL"
 
 #define your tesseract.exe file path
 path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -12,17 +19,34 @@ def convertImage2Text(img):
     text = pytesseract.image_to_string(img) 
     return text[:-1]
     
-def convertText2Field(img2text):
+def convertText2Field(img2text,file):
     
     #extract value of ringgit
     textRM = re.search("RM( {0,1}\d{1,5}\.\d{2})",img2text)
-    valueRM = textRM.group(1).replace(' ','')
-    Rm.append(valueRM)
+    if textRM:
+        valueRM = textRM.group(1).replace(' ','')
+        Rm.append(valueRM)
+    else:
+        Rm.append(null)
+        fileNameError(file)
+    
     
     #extract value of date
     textDate = re.search ("(\d{1,2} \w{3} 20\d{2})", img2text)
-    textDate = textDate.group(1).replace(' ','')
-    date.append(textDate)
+    if textDate:
+        textDate = textDate.group(1).replace(' ','')
+        date.append(textDate)
+    else:
+        date.append(null)
+    
+    #testing    
+    merchant = re.search ("Merchant Name\n([^\n]*)", img2text)
+    if merchant:
+        merchant = merchant.group(1)
+        merchantName.append(merchant)
+    else:
+        merchantName.append(null)
+    
     
 #_____________________________________________________________extract value of merchant_____________________________________________________________________________
 
@@ -33,14 +57,14 @@ def convertText2Field(img2text):
         1. Merchant QR
         2. Personal QR
     '''
-    
+    '''
     try:
         merchant = re.search ("Merchant Name\n([^\n]*)", img2text)
         merchantName.append(merchant.group(1))
     except AttributeError as e:
         print("not match")
     except ValueError as e:
-        print("value error")
+        print("value error")'''
 
       
     
@@ -49,16 +73,14 @@ if __name__=="__main__":
     directory_path = r"C:\Users\firex\OneDrive\Documents\duitNow\sample image"
     png_files = glob.glob(os.path.join(directory_path, "*.png"))
     
-    Rm=[]
-    date = []
-    merchantName= []
+
     
     for file in png_files:
         img2text = convertImage2Text(file)
         
-        toList= convertText2Field(img2text=img2text)
+        toList= convertText2Field(img2text=img2text,file=file)
         
-    rint(merchantName)
+    print(merchantName)
         
     
         
